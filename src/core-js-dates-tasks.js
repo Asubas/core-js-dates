9776199dc1a6877f90c5a4c6ea20c9b2e4f06968
Леха <znaxor74@mail.ru>
2024-03-20 08:28:40 +0300

    fix: implement getQuarter

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -264,8 +264,14 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const currentMonth = date.getMonth();
+  let quarter = 0;
+  if (currentMonth >= 0 && currentMonth < 3) quarter = 1;
+  if (currentMonth >= 3 && currentMonth < 6) quarter = 2;
+  if (currentMonth >= 6 && currentMonth < 9) quarter = 3;
+  if (currentMonth >= 9 && currentMonth < 12) quarter = 4;
+  return quarter;
 }
 
 /**
--- testt.js
+++ testt.js
@@ -1,17 +1,11 @@
-// function getNextFridayThe13th(date) {
-//   const currentDay = date.getDay();
-//   let shift;
-//   console.log(date.getDay());
-//   if (currentDay === 6) {
-//     shift = 6;
-//   } else {
-//     shift = 5 - currentDay;
-//   }
-//   date.setDate(date.getDate() + shift);
-//   while (date.getDate() !== 13) {
-//     date.setDate(date.getDate() + 7);
-//   }
-//   return date;
-// }
+function getQuarter(date) {
+  const currentMonth = date.getMonth();
+  let quarter = 0;
+  if (currentMonth >= 0 && currentMonth < 3) quarter = 1;
+  if (currentMonth >= 3 && currentMonth < 6) quarter = 2;
+  if (currentMonth >= 6 && currentMonth < 9) quarter = 3;
+  if (currentMonth >= 9 && currentMonth < 12) quarter = 4;
+  return quarter;
+}
 
-// console.log(getNextFridayThe13th(new Date(2024, 0, 13)));
+console.log(getQuarter(new Date(2024, 10, 10)));
